355ba8d183aed873d3c6fa343aeeaaf6745d09b0
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-05-12 17:29:50 +0300

    Add project files.

---
+++ Booksaw.Entity/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booksaw.Entity
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
